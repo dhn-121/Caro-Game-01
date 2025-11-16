1bc274193b449c5936caf8719d266c85f07962ee
Nguyen Hoang Chau <5nhchau10a9@gmail.com>
2025-11-16 20:10:51 +0700

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -16,63 +16,74 @@ void FixConsoleWindow() {
 void DrawBoard() {
     const int cellWidth = 8;  // tăng khoảng ngang để vuông hơn
     const int cellHeight = 3; // chiều cao mỗi ô
+    int current_Y = Yi;
 
     // Hàng đầu tiên
+    setPos(Xi, current_Y);
     cout << char(218);
     for (int j = 0; j < BOARD_SIZE- 1; j++) {
         for (int k = 0; k < cellWidth; k++) cout << char(196);
         cout << char(194);
     }
     for (int k = 0; k < cellWidth; k++) cout << char(196);
-    cout << char(191) << endl;
+    cout << char(191);
+    current_Y++;
 
-    // Thân 
+    // Thân
     for (int i = 0; i < BOARD_SIZE - 1; i++) {
         for (int h = 0; h < cellHeight; h++) {
+            setPos(Xi, current_Y);
             cout << char(179);
             for (int j = 0; j < BOARD_SIZE; j++) {
                 for (int k = 0; k < cellWidth; k++) cout << ' ';
                 cout << char(179);
             }
-            cout << endl;
+            current_Y++;
         }
-
+        setPos(Xi, current_Y);
         cout << char(195);
         for (int j = 0; j < BOARD_SIZE - 1; j++) {
             for (int k = 0; k < cellWidth; k++) cout << char(196);
             cout << char(197);
         }
         for (int k = 0; k < cellWidth; k++) cout << char(196);
-        cout << char(180) << endl;
+        cout << char(180);
+        current_Y++;
     }
 
     // Hàng cuối
     for (int h = 0; h < cellHeight; h++) {
+        setPos(Xi, current_Y);
         cout << char(179);
         for (int j = 0; j < BOARD_SIZE; j++) {
             for (int k = 0; k < cellWidth; k++) cout << ' ';
             cout << char(179);
         }
-        cout << endl;
+        current_Y++;
     }
 
+    setPos(Xi, current_Y);
     cout << char(192);
     for (int j = 0; j < BOARD_SIZE - 1; j++) {
         for (int k = 0; k < cellWidth; k++) cout << char(196);
         cout << char(193);
     }
     for (int k = 0; k < cellWidth; k++) cout << char(196);
-    cout << char(217) << endl;
+    cout << char(217);
+    setPos(Xi, current_Y);
 }
 
 
-int main() {
-    FixConsoleWindow();
-    DrawBoard();
-    cout << "\nBan co da ve xong!\n";
-    cin.get();
-    return 0;
-}
+// int main() {
+//     FixConsoleWindow();
+//     DrawBoard();
+//     cout << "\nBan co da ve xong!\n";
+//     cin.get();
+//     return 0;
+// }
+
+
+
 
 
 
