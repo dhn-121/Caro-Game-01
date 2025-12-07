2417a91f53823a6150880e30e536209ffb8aabf6
Ngọc Hà Trương <truongthingocha2007@gmail.com>
2025-12-07 21:16:51 +0700

    Delete checkcaro.h

--- checkcaro.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef _CHECKCARO_H_
-#define _CHECKCARO_H_
-#include "Library.h"
-#include "Graphics.h"
-void getij(int&, int&, int, int);
-bool check_iswin(int, int, char[N][N]);
-#endif
