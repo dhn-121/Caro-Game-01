ebe26ff7c8556b586711348840fb23a688e930e9
Nguyen Hoang Chau <5nhchau10a9@gmail.com>
2025-12-12 00:43:40 +0700

    Refactor drawing functions and remove drawSETTING
    Removed the drawSETTING function and added drawS function for drawing a specific character. Updated drawPAUSE and drawSAVELOAD functions to include new drawing logic.

--- Character.cpp
+++ Character.cpp
@@ -1,4 +1,4 @@
-﻿#include "Library.h"
+#include "Library.h"
 
 using namespace std;
 
@@ -646,15 +646,6 @@ void drawDRAW(int XX, int YY) {
 	drawW(XX + 24, YY);
 }
 
-void drawSETTING(int XX, int YY) {
-	//drawS(XX, YY);
-	drawE(XX + 5, YY);
-	drawT(XX + 13, YY);
-	drawT(XX + 22, YY);
-	drawI(XX + 31, YY);
-	drawN(XX + 34, YY);
-	drawG(XX + 44, YY);
-}
 void drawGAME_MODE(int XX, int YY) {
 	drawG(XX, YY);
 	drawA(XX + 9, YY);
@@ -905,4 +896,416 @@ void drawDino(int XX, int YY) {
 		cout << B;
 	}*/
 
+}
+
+void drawS(int XX, int YY) {
+	setPos(XX, YY);
+	setColor(0, 0); cout << "       ";
+	setColor(co_theme); cout << u8"╗";
+
+	setPos(XX, YY + 1);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"╔";
+	for (int i = 0; i < 4; ++i) cout << u8"═";
+	cout << u8"╝";
+
+	setPos(XX, YY + 2);
+	setColor(0, 0); cout << "       ";
+	setColor(co_theme); cout << u8"╗";
+
+	setPos(XX + 5, YY + 3);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"║";
+
+	setPos(XX, YY + 4);
+	setColor(0, 0); cout << "       ";
+	setColor(co_theme); cout << u8"║";
+
+	setPos(XX, YY + 5);
+	cout << u8"╚";
+	for (int i = 0; i < 6; ++i) cout << u8"═";
+	cout << u8"╝";
+
+
+}
+
+void drawV(int XX, int YY) {
+	setPos(XX, YY);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"╗" << "    ";
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"╗";
+
+	setPos(XX, YY + 1);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"║" << "    ";
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"║";
+	
+
+	setPos(XX, YY + 2);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"║" << "    ";
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"║";
+
+	setPos(XX+1, YY + 3);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"╗" << "  ";
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout << u8"╝";
+	
+	setPos(XX + 3, YY + 4);
+	setColor(0, 0); cout << "  ";
+	setColor(co_theme); cout  << "";
+	setColor(0, 0); cout << " ";
+	setColor(co_theme); cout << u8"╝";
+	
+
+	setPos(XX + 3, YY + 5);
+	cout << u8"╚"  << u8"═" << u8"╝";
+	
+}
+
+
+void drawSAVELOAD(int XX, int YY) {
+	drawS(XX, YY);
+	drawA(XX + 8, YY);
+	drawV(XX + 16, YY);
+	drawE(XX + 26, YY);
+
+	drawL(XX + 40, YY);
+	drawO(XX + 48, YY);
+	drawA(XX + 57, YY);
+	drawD(XX + 65, YY);
+}
+
+void drawPAUSE(int XX, int YY) {
+	drawP(XX, YY);
+	drawA(XX + 8, YY);
+	drawU(XX + 16, YY);
+	drawS(XX + 26, YY);
+	drawE(XX + 34, YY);
+}
+void drawSETTING(int XX, int YY) {
+	drawS(XX - 14 , YY);
+	drawE(XX -6, YY);
+	drawT(XX + 2, YY);
+	drawT(XX + 11, YY);
+	drawI(XX + 20, YY);
+	drawN(XX + 23, YY);
+	drawG(XX + 33, YY);
+	drawS(XX + 42, YY);
+}
+void drawCactus(int XX, int YY) {
+	const char* A = u8"█ ";
+	const char* B = u8"██ ";          // khối 2 ô
+	const char* K = u8"████████ ";
+
+	// ======== ROW 8 =========
+	setPos(XX + 94, YY + 8); cout << B;
+	setPos(XX + 96, YY + 8); cout << B;
+
+
+	// ======== ROW 9 =========
+	setPos(XX + 93, YY + 9); cout << B;
+	setPos(XX + 94, YY + 9); cout << B;
+	setPos(XX + 96, YY + 9); cout << B;
+	setPos(XX + 97, YY + 9); cout << B;
+
+
+	// ======== ROW 10 =========
+	setPos(XX + 93, YY + 10); cout << B;
+	setPos(XX + 94, YY + 10); cout << B;
+	setPos(XX + 96, YY + 10); cout << B;
+	setPos(XX + 97, YY + 10); cout << B;
+
+
+	// ======== ROW 11 =========
+	setPos(XX + 93, YY + 11); cout << B;
+	setPos(XX + 94, YY + 11); cout << B;
+	setPos(XX + 96, YY + 11); cout << B;
+	setPos(XX + 97, YY + 11); cout << B;
+
+
+	// ======== ROW 12 - canh phai =========
+	setPos(XX + 93, YY + 12); cout << B;
+	setPos(XX + 94, YY + 12); cout << B;
+	setPos(XX + 96, YY + 12); cout << B;
+	setPos(XX + 97, YY + 12); cout << B;
+	//setPos(XX + 103, YY + 12); cout << B;
+	setPos(XX + 104, YY + 12); cout << B;
+	setPos(XX + 105, YY + 12); cout << B;
+
+
+	// ======== ROW 13 - canh trai =========
+	setPos(XX + 85, YY + 13); cout << B;
+	setPos(XX + 86, YY + 13); cout << B;
+	setPos(XX + 93, YY + 13); cout << B;
+	setPos(XX + 94, YY + 13); cout << B;
+	setPos(XX + 96, YY + 13); cout << B;
+	setPos(XX + 97, YY + 13); cout << B;
+	setPos(XX + 103, YY + 13); cout << B;
+	setPos(XX + 104, YY + 13); cout << B;
+	setPos(XX + 105, YY + 13); cout << B;
+	setPos(XX + 106, YY + 13); cout << B;
+
+	// ======== ROW 14 =========
+	setPos(XX + 84, YY + 14); cout << B;
+	setPos(XX + 86, YY + 14); cout << B;
+	setPos(XX + 87, YY + 14); cout << B;
+	setPos(XX + 93, YY + 14); cout << B;
+	setPos(XX + 94, YY + 14); cout << B;
+	setPos(XX + 96, YY + 14); cout << B;
+	setPos(XX + 97, YY + 14); cout << B;
+	setPos(XX + 103, YY + 14); cout << B;
+	setPos(XX + 104, YY + 14); cout << B;
+	setPos(XX + 105, YY + 14); cout << B;
+	setPos(XX + 106, YY + 14); cout << B;
+
+
+	// ======== ROW 15 =========
+	setPos(XX + 84, YY + 15); cout << B;
+	setPos(XX + 86, YY + 15); cout << B;
+	setPos(XX + 87, YY + 15); cout << B;
+	setPos(XX + 93, YY + 15); cout << B;
+	setPos(XX + 94, YY + 15); cout << B;
+	setPos(XX + 96, YY + 15); cout << B;
+	setPos(XX + 97, YY + 15); cout << B;
+	setPos(XX + 103, YY + 15); cout << B;
+	setPos(XX + 104, YY + 15); cout << B;
+	setPos(XX + 105, YY + 15); cout << B;
+	setPos(XX + 106, YY + 15); cout << B;
+
+	// ======== ROW 16 =========
+	setPos(XX + 84, YY + 16); cout << B;
+	setPos(XX + 86, YY + 16); cout << B;
+	setPos(XX + 87, YY + 16); cout << B;
+	setPos(XX + 93, YY + 16); cout << B;
+	setPos(XX + 94, YY + 16); cout << B;
+	setPos(XX + 96, YY + 16); cout << B;
+	setPos(XX + 97, YY + 16); cout << B;
+	setPos(XX + 103, YY + 16); cout << B;
+	setPos(XX + 104, YY + 16); cout << B;
+	setPos(XX + 105, YY + 16); cout << B;
+	setPos(XX + 106, YY + 16); cout << B;
+
+	// ======== ROW 17 =========
+	setPos(XX + 84, YY + 17); cout << B;
+setPos(XX + 86, YY + 17); cout << B;
+	setPos(XX + 87, YY + 17); cout << B;
+	setPos(XX + 93, YY + 17); cout << B;
+	setPos(XX + 94, YY + 17); cout << B;
+	setPos(XX + 96, YY + 17); cout << B;
+	setPos(XX + 97, YY + 17); cout << B;
+	setPos(XX + 103, YY + 17); cout << B;
+	setPos(XX + 104, YY + 17); cout << B;
+	setPos(XX + 105, YY + 17); cout << B;
+	setPos(XX + 106, YY + 17); cout << B;
+
+
+	// ======== ROW 18 - noi =========
+	setPos(XX + 84, YY + 18); cout << B;
+	setPos(XX + 86, YY + 18); cout << B;
+	setPos(XX + 88, YY + 18); cout << B;
+	setPos(XX + 90, YY + 18); cout << B;
+	setPos(XX + 92, YY + 18); cout << B;
+	setPos(XX + 93, YY + 18); cout << B;
+	setPos(XX + 94, YY + 18); cout << B;
+	setPos(XX + 96, YY + 18); cout << B;
+	setPos(XX + 97, YY + 18); cout << B;
+	setPos(XX + 99, YY + 18); cout << B;
+	setPos(XX + 101, YY + 18); cout << B;
+	setPos(XX + 103, YY + 18); cout << B;
+	setPos(XX + 104, YY + 18); cout << B;
+	setPos(XX + 105, YY + 18); cout << B;
+	setPos(XX + 106, YY + 18); cout << B;
+
+	// ======== ROW 19 =========
+	setPos(XX + 85, YY + 19); cout << B;
+	setPos(XX + 86, YY + 19); cout << B;
+	setPos(XX + 88, YY + 19); cout << B;
+	setPos(XX + 90, YY + 19); cout << B;
+	setPos(XX + 92, YY + 19); cout << B;
+	setPos(XX + 93, YY + 19); cout << B;
+	setPos(XX + 94, YY + 19); cout << B;
+	setPos(XX + 96, YY + 19); cout << B;
+	setPos(XX + 97, YY + 19); cout << B;
+	setPos(XX + 99, YY + 19); cout << B;
+	setPos(XX + 101, YY + 19); cout << B;
+	setPos(XX + 103, YY + 19); cout << B;
+	setPos(XX + 104, YY + 19); cout << B;
+	//setPos(XX + 105, YY + 19); cout << B;
+
+	// ======== ROW 20 =========
+	setPos(XX + 87, YY + 20); cout << B;
+	setPos(XX + 88, YY + 20); cout << B;
+	setPos(XX + 90, YY + 20); cout << B;
+	setPos(XX + 92, YY + 20); cout << B;
+	setPos(XX + 93, YY + 20); cout << B;
+	setPos(XX + 94, YY + 20); cout << B;
+	setPos(XX + 96, YY + 20); cout << B;
+	setPos(XX + 97, YY + 20); cout << B;
+	setPos(XX + 99, YY + 20); cout << B;
+	setPos(XX + 101, YY + 20); cout << B;
+	setPos(XX + 102, YY + 20); cout << B;
+	//setPos(XX + 104, YY + 20); cout << B;
+
+
+
+	//========= ROW 21 - only than ======= 
+	setPos(XX + 93, YY + 21); cout << B;
+	setPos(XX + 94, YY + 21); cout << B;
+	setPos(XX + 96, YY + 21); cout << B;
+	setPos(XX + 97, YY + 21); cout << B;
+
+	//========= ROW 22  ======= 
+	setPos(XX + 93, YY + 22); cout << B;
+	setPos(XX + 94, YY + 22); cout << B;
+	setPos(XX + 96, YY + 22); cout << B;
+	setPos(XX + 97, YY + 22); cout << B;
+
+	//========= ROW 23 ======= 
+	setPos(XX + 93, YY + 23); cout << B;
+	setPos(XX + 94, YY + 23); cout << B;
+	setPos(XX + 96, YY + 23); cout << B;
+	setPos(XX + 97, YY + 23); cout << B;
+
+	//========= ROW 24 ======= 
+	setPos(XX + 93, YY + 24); cout << B;
+	setPos(XX + 94, YY + 24); cout << B;
+	setPos(XX + 96, YY + 24); cout << B;
+	setPos(XX + 97, YY + 24); cout << B;
+
+	//========= ROW 25 ======= 
+	setPos(XX + 93, YY + 25); cout << B;
+	setPos(XX + 94, YY + 25); cout << B;
+	setPos(XX + 96, YY + 25); cout << B;
+	setPos(XX + 97, YY + 25); cout << B;
+//========= ROW 26 ======= 
+	setPos(XX + 93, YY + 26); cout << B;
+	setPos(XX + 94, YY + 26); cout << B;
+	setPos(XX + 96, YY + 26); cout << B;
+	setPos(XX + 97, YY + 26); cout << B;
+
+	// ========= ROW 21 mat dat ========
+	for (int i = 2; i < 119; i++) {
+		setPos(XX + i, YY + 28);
+		cout << B;
+	}
+}
+
+void drawMiniDino(int XX, int YY) {
+	const char* A = u8"█ ";
+	const char* B = u8"██ ";
+	
+
+	setPos(XX + 68, YY + 11); cout << B;
+	setPos(XX + 70, YY + 11); cout << B;
+	setPos(XX + 72, YY + 11); cout << B;
+	setPos(XX + 74, YY + 11); cout << B;
+	setPos(XX + 76, YY + 11); cout << B;
+	
+	
+	setPos(XX + 68, YY + 12); cout << B;
+	setPos(XX + 70, YY + 12); cout << B;
+	setPos(XX + 71, YY + 12); cout << "  ";
+	setPos(XX + 72, YY + 12); cout << B;
+	setPos(XX + 74, YY + 12); cout << B;
+	setPos(XX + 76, YY + 12); cout << B;
+	
+	
+	setPos(XX + 68, YY + 13); cout << B;
+	setPos(XX + 70, YY + 13); cout << B;
+	setPos(XX + 72, YY + 13); cout << B;
+    setPos(XX + 74, YY + 13); cout << B;
+	setPos(XX + 76, YY + 13); cout << B;
+	
+	
+	setPos(XX + 63, YY + 14); cout << B; 
+	setPos(XX + 68, YY + 14); cout << B;
+	setPos(XX + 70, YY + 14); cout << B;
+	setPos(XX + 72, YY + 14); cout << B;
+	//setPos(XX + 75, YY + 14); cout << A;
+
+
+	//setPos(XX + 61, YY + 15); cout << B;
+	setPos(XX + 63, YY + 15); cout << B;
+	setPos(XX + 65, YY + 15); cout << B;
+	//setPos(XX + 67, YY + 15); cout << B;
+	setPos(XX + 68, YY + 15); cout << B;
+	setPos(XX + 70, YY + 15); cout << B;
+	setPos(XX + 72, YY + 15); cout << B;
+	setPos(XX + 74, YY + 15); cout << B;
+	setPos(XX + 75, YY + 15); cout << A;
+
+	setPos(XX + 64, YY + 16); cout << B;
+	setPos(XX + 65, YY + 16); cout << B;
+	setPos(XX + 67, YY + 16); cout << B;
+	setPos(XX + 69, YY + 16); cout << B;
+	setPos(XX + 70, YY + 16); cout << B;
+	setPos(XX + 72, YY + 16); cout << B;
+
+	setPos(XX + 65, YY + 17); cout << B;
+	setPos(XX + 67, YY + 17); cout << B;
+	setPos(XX + 69, YY + 17); cout << B;
+	setPos(XX + 70, YY + 17); cout << B;
+	setPos(XX + 72, YY + 17); cout << A;
+
+	setPos(XX + 66, YY + 18); cout << B;
+	setPos(XX + 69, YY + 18); cout << B;
+
+	for (int i = 70; i < 118; i++) {
+		setPos(XX - 10 + i, YY + 19);
+		cout << B;
+	}
+
+}
+
+void drawMiniCactus(int XX, int YY) {
+	const char* A = u8"█ ";
+	const char* B = u8"██ ";
+
+
+	setPos(XX + 94, YY + 9); cout << B;
+	setPos(XX + 95, YY + 9); cout << A;
+
+	setPos(XX + 94, YY + 10); cout << B;
+	setPos(XX + 95, YY + 10); cout << A;
+	setPos(XX + 100, YY + 10); cout << B;
+
+	setPos(XX + 94, YY + 11); cout << B;
+	setPos(XX + 95, YY + 11); cout << A;
+	setPos(XX + 100, YY + 11); cout << B;
+
+	setPos(XX + 90, YY + 12); cout << B;
+	setPos(XX + 94, YY + 12); cout << B;
+	setPos(XX + 95, YY + 12); cout << A;
+	setPos(XX + 100, YY + 12); cout << B;
+
+	setPos(XX + 90, YY + 12); cout << B;
+	setPos(XX + 94, YY + 12); cout << B;
+	setPos(XX + 95, YY + 12); cout << A;
+
+	setPos(XX + 90, YY + 13); cout << B;
+	setPos(XX + 94, YY + 13); cout << B;
+	setPos(XX + 95, YY + 13); cout << B;
+	setPos(XX + 97, YY + 13); cout << B;
+	setPos(XX + 99, YY + 13); cout << B;
+	
+	setPos(XX + 90, YY + 14); cout << B;
+	setPos(XX + 94, YY + 14); cout << B;
+	setPos(XX + 95, YY + 14); cout << A;
+
+	setPos(XX + 91, YY + 15); cout << B;
+	setPos(XX + 92, YY + 15); cout << B;
+	setPos(XX + 94, YY + 15); cout << B;
+	setPos(XX + 95, YY + 15); cout << A;
+
+	setPos(XX + 94, YY + 16); cout << B;
+	setPos(XX + 95, YY + 16); cout << A;
+
+	setPos(XX + 94, YY + 17); cout << B;
+	setPos(XX + 95, YY + 17); cout << A;
+	
 }
